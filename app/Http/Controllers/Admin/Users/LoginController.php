cda687a6b29c6030b5d676e0a6aa39e847b4dbf2
Sinoo <khoasinno@gmail.com>
2024-09-12 13:54:05 +0700

    validate - alert error - session alert - auth::login

--- app/Http/Controllers/Admin/Users/LoginController.php
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
@@ -12,5 +14,25 @@ class LoginController extends Controller
         return view('admin.users.login', [
             'title' => 'Login',
         ]);
+    }
+    public function store(Request $request)
+    {
+        // dd($request->input());
+        $this->validate($request, [
+            'email' => 'required|email:filter',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt([
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ], $request->input('remember'))) {
+
+            return redirect()->route('admin');
+        } else {
+            Session::flash('error', 'Email or password incorrect');
+        };
+
+        return redirect()->back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\Users\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('admin/users/login', [LoginController::class, 'index']);
+Route::post('/admin/users/login/store', [LoginController::class, 'store']);
+
+
+
+Route::get('/admin/main', [MainController::class, 'index'])->name('admin');
